5c635454c6b6d0fe83bc9b47315718254ac9cc10
Nelu Cristian Tone <39335855+nelucristian@users.noreply.github.com>
2020-08-30 16:05:39 +0300

    Add a warning message if it's missing -Dgoogle.api.key from VM arguments

--- src/main/java/exam/veracityid/VeracityidApplication.java
+++ src/main/java/exam/veracityid/VeracityidApplication.java
@@ -1,6 +1,7 @@
 package exam.veracityid;
 
 import kong.unirest.Unirest;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VeracityidApplication {
 
     public static void main(String[] args) {
+        final String googleApiKey = System.getProperty("google.api.key");
+        if (Strings.isBlank(googleApiKey)){
+            System.err.println("You need to start the server with -Dgoogle.api.key=<your_key_for_google_places_api>");
+            return;
+        }
         Unirest.config().addShutdownHook(true);
         SpringApplication.run(VeracityidApplication.class, args);
     }
